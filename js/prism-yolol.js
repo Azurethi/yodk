313f84ebc9bf862a5dc26931df38e3c0b9f51638
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-01 16:05:47 +0000

    Deploy dbaumgarten/yodk to github.com/dbaumgarten/yodk.git:gh-pages

---
+++ js/prism-yolol.js
@@ -0,0 +1,9 @@
+Prism.languages.yolol = {
+    'comment': /\/\/.+/,
+    'string': /"[^"]*"/,
+    'keyword': /\b(if|then|else|end|goto|abs|sqrt|sin|cos|tan|asin|acos|atan)\b/i,
+    'operator': /\b(and|or|not)\b/i,
+    'function': /[a-z0-9_]+(?=\()/i,
+    'variable': /:?[a-zA-Z]+[a-zA-Z0-9_]*/,
+    'constant': /[0-9]+(\.[0-9]+)?/
+};
